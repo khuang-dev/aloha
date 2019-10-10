adb11d2efff6405c294edbeb251a21b6d3358397
khuang-dev <khuang.dev@gmail.com>
2019-10-10 15:59:00 -0700

    Added flickity for 600px,1240px,max, removed prev-next button

---
+++ css/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() { 
+// all of the codes go inside here
+const elem = document.querySelector('.main-carousel');
+const flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  wrapAround: true,
+  freeScroll: true
+});
+
+ }); //end of script
