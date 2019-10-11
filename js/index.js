957fd62a6f2669c844a14316ff48d3e06829162c
khuang-dev <khuang.dev@gmail.com>
2019-10-11 15:47:27 -0700

    added add to cart buttons

--- js/index.js
+++ js/index.js
@@ -1,33 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() { 
-// all of the codes go inside here
-
+//Flickity
 const elem = document.querySelector('.main-carousel');
 const flkty = new Flickity( elem, {
-  // options
   cellAlign: 'left',
   wrapAround: true,
   freeScroll: true,
   autoPlay: true
 });
+//Fixed Header
+
+});
+//Add to cart
+
 
 const emailForm = document.getElementById("emailform");
-
 const emailInput = document.getElementById("youremail");
-// const emailValue = emailInput.value;
-console.log(emailInput);
-// console.log(emailValue,emailInput.value);
 
 emailForm.addEventListener("submit", function(event) {
     event.preventDefault();
     if ( emailInput.value == ""){
-        alert("Please submit a valid email address") //true
+        alert("Please submit a valid email address")
     } else {
-        alert("Thanks for subscribing!") //false
+        alert("Thanks for subscribing!")
     }
 });
 
-
-
-
-
- }); //end of script
