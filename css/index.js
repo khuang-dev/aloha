10aa6a33ea5ebd25d2e202451fa327b3bf632813
khuang-dev <khuang.dev@gmail.com>
2019-10-10 18:28:47 -0700

    email form messages added

--- css/index.js
+++ css/index.js
@@ -1,11 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() { 
 // all of the codes go inside here
+
 const elem = document.querySelector('.main-carousel');
 const flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
   wrapAround: true,
-  freeScroll: true
+  freeScroll: true,
+  autoPlay: true
 });
+
+const emailForm=document.getElementById("emailform");
+const emailInput=document.getElementById("youremail").value;
+const emailValue = emailInput.value;
+console.log(emailValue);
+emailForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    if (emailValue == ""){
+        event.preventDefault();
+        alert("Please submit a valid email address")
+    } else {
+        event.preventDefault();
+        alert("Thanks for subscribing!")
+    }
+})
+
+
+
+
 
  }); //end of script
