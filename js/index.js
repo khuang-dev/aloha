38023a93d03d0addebcf0413b1b4fb8e2881e211
khuang-dev <khuang.dev@gmail.com>
2019-10-15 15:58:19 -0700

    add to cart and counter complete

--- js/index.js
+++ js/index.js
@@ -1,37 +1,42 @@
-document.addEventListener("DOMContentLoaded", function() { 
-//Flickity
-const elem = document.querySelector('.main-carousel');
-const flkty = new Flickity( elem, {
-  cellAlign: 'left',
-  wrapAround: true,
-  freeScroll: true,
-  autoPlay: true
-});
-//Fixed Header
+document.addEventListener("DOMContentLoaded", function () {
+    //Flickity
+    const elem = document.querySelector('.main-carousel');
+    const flkty = new Flickity(elem, {
+        cellAlign: 'left',
+        wrapAround: true,
+        freeScroll: true,
+        autoPlay: true
+    });
+    //Fixed Header
+
+    //Add to cart
+    const shoppingCartButton = document.getElementById("shopping-cart-button");
+    const addToCart = document.querySelectorAll(".addtocart");
+    let counter = 0;
+    
+    shoppingCartButton.style.display = "none";
+    for (i = 0; i < addToCart.length; i++) {
+        addToCart[i].addEventListener("click", function () {
+            counter++;
+            shoppingCartButton.innerText = counter;
+            if (counter > 0) {
+                shoppingCartButton.style.display = "block";
+            }
+        })
+    };
+
+    //Email
+
+    const emailForm = document.getElementById("emailform");
+    const emailInput = document.getElementById("youremail");
+
+    emailForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        if (emailInput.value == "") {
+            alert("Please submit a valid email address")
+        } else {
+            alert("Thanks for subscribing!")
+        }
+    });
 
 });
-//Add to cart
-const shoppingCartButton = document.getElementById("shopping-cart-button");
-const addToCart = document.getElementById("addtocart");
-let counter=0;
-
-
-
-addToCart.addEventListener("click" , function() {
-    counter ++;
-    }
-);
-//Email
-
-const emailForm = document.getElementById("emailform");
-const emailInput = document.getElementById("youremail");
-
-emailForm.addEventListener("submit", function(event) {
-    event.preventDefault();
-    if ( emailInput.value == ""){
-        alert("Please submit a valid email address")
-    } else {
-        alert("Thanks for subscribing!")
-    }
-});
-
