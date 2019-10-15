470582908d932ffcf04321dcc6e5a95d28f3b2a8
khuang-dev <khuang.dev@gmail.com>
2019-10-15 13:49:55 -0700

    added add to cart icon css decorations

--- js/index.js
+++ js/index.js
@@ -11,7 +11,17 @@ const flkty = new Flickity( elem, {
 
 });
 //Add to cart
+const shoppingCartButton = document.getElementById("shopping-cart-button");
+const addToCart = document.getElementById("addtocart");
+let counter=0;
 
+
+
+addToCart.addEventListener("click" , function() {
+    counter ++;
+    }
+);
+//Email
 
 const emailForm = document.getElementById("emailform");
 const emailInput = document.getElementById("youremail");
